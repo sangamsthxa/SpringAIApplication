a185b4bda7650ad91a36f2441cad69ad9819e541
sangam <sangam.shrestha1997@gmail.com>
2025-06-24 11:37:52 -0700

    Asking Question to Open AI model and working with Chat Client, Chat Response and MetaData

--- src/main/java/org/sangam/springaicode/controller/OpenAIController.java
+++ src/main/java/org/sangam/springaicode/controller/OpenAIController.java
@@ -1,5 +1,9 @@
 package org.sangam.springaicode.controller;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +11,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OpenAIController {
 
+    private OpenAiChatModel openAiChatModel;
+    private ChatClient chatClient;
+
+    public OpenAIController(OpenAiChatModel openAiChatModel) {
+        this.openAiChatModel = openAiChatModel;
+        this.chatClient = ChatClient.create(openAiChatModel);
+    }
+
     @GetMapping("/api/{message}")
     public String getAnswer(@PathVariable String message){
-        return "Hello " + message;
+
+        String response = openAiChatModel.call(message);
+        return response;
     }
+
+    @GetMapping("/api/chatClient/{message}")
+    public ResponseEntity<String> getChatClientAnswer(@PathVariable String message){
+
+        String response = chatClient.prompt(message).call().content();
+        ChatResponse chatResponse = chatClient.prompt(message).call().chatResponse();
+        System.out.println(chatResponse.getMetadata().getModel());
+        response = chatResponse
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return ResponseEntity.ok(response);
+    }
+
+
 
 }
